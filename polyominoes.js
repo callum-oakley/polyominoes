00b14e1f165f0183bfaf09de92c266cde49eb5c0
Callum Oakley <callum@pusher.com>
2017-05-21 23:35:29 +0100

    nearly there, tiny bit buggy

---
+++ polyominoes.js
@@ -0,0 +1,84 @@
+const transformations = [
+  ([ x, y ]) => [  x,  y ], // identity
+  ([ x, y ]) => [ -y,  x ], // rotate pi/2
+  ([ x, y ]) => [ -x, -y ], // rotate pi
+  ([ x, y ]) => [  y, -x ], // rotate 3pi/2
+  ([ x, y ]) => [ -x,  y ], // reflect in x axis
+  ([ x, y ]) => [  x, -y ], // reflect in y axis
+  ([ x, y ]) => [  y,  x ], // reflect in x = y
+  ([ x, y ]) => [ -y,  x ]  // reflect in x = -y
+];
+
+class Polyomino {
+  // Reduces a representation of a polyomino to a string such that two
+  // representations of the same polyomino (up to translation) will produce the
+  // same string
+  static normalize(representation) {
+    const dx = Math.min(...representation.map(([ x, y ]) => x));
+    const dy = Math.min(...representation.map(([ x, y ]) => y));
+    // Make the co-ordinates as small as possible, while all still positive,
+    // sort, and stringify
+    return JSON.stringify(representation.map(([ x, y ]) => [ x - dx, y - dy ]).sort());
+  }
+
+  constructor(representation) {
+    // Store a set of the normalized representations of each rotation and
+    // reflection of the polyomino
+    this.representations = new Set(
+      transformations.map(t => representation.map(square => t(square)))
+        .map(Polyomino.normalize)
+    );
+  }
+
+  // Tests if the polynomio is represented by the provided list of squares
+  isRepresentedBy(representation) {
+    return this.representations.has(Polyomino.normalize(representation));
+  }
+
+  // Gets an arbitrary represenation of the polynomio
+  get representation() {
+    return JSON.parse(this.representations.values().next().value);
+  }
+}
+
+const directions = [ [ 1, 0 ], [ 0, 1 ], [ -1, 0 ], [ 0, -1 ] ];
+
+function containsSquare(representation, square) {
+  return representation.some(s => JSON.stringify(s) === JSON.stringify(square));
+}
+
+// Returns an iterable of representations that can be built by adding one
+// square to the given representation
+function* grow(representation) {
+  for ([ x, y ] of representation) {
+    for ([ dx, dy ] of directions) {
+      if (!containsSquare(representation, [ x + dx, y + dy ])) {
+        yield [ ...representation, [ x + dx, y + dy ] ]
+      }
+    }
+  }
+}
+
+// Tests if the provided representation corresponds to one of the provided
+// polyominoes
+function alreadyRepresented(polyominoes, representation) {
+  return polyominoes.some(p => p.isRepresentedBy(representation));
+}
+
+// Returns all free polyominoes consisting of n or fewer squares
+function polyominoes(n) {
+  const ominoes = { 1: [ new Polyomino([ [ 0, 0 ] ]) ] };
+  for (let i = 2; i <= n; i++) {
+    ominoes[i] = [];
+    for (omino of ominoes[i - 1]) {
+      for (representation of grow(omino.representation)) {
+        if (!alreadyRepresented(ominoes[i], representation)) {
+          ominoes[i].push(new Polyomino(representation));
+        }
+      }
+    }
+  }
+  return ominoes;
+}
+
+module.exports = polyominoes;
