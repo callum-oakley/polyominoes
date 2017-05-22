d13c77fb2e0a8ea4346867fed627aa7d2c0124ab
Callum Oakley <callum@pusher.com>
2017-05-22 12:57:29 +0100

    finished python version (and removed js)

--- polyominoes.py
+++ polyominoes.py
@@ -8,20 +8,43 @@ def normalize(representation):
     dy = min(y for (_, y) in representation)
     return frozenset((x - dx, y - dy) for (x, y) in representation)
 
-# It makes things easier later if we store a polyomino as a set of the
-# normalized representations of all its rotations and reflections.
+# We'll store a polyomino as a set of the normalized representations of all its
+# rotations and reflections.
 
-def polyomino(representation):
+def newPolyomino(representation):
     transformations = [
-        lambda s: ( s[0],  s[1]),
-        lambda s: (-s[1],  s[0]),
-        lambda s: (-s[0], -s[1]),
-        lambda s: ( s[1], -s[0]),
-        lambda s: (-s[0],  s[1]),
-        lambda s: ( s[0], -s[1]),
-        lambda s: ( s[1],  s[0]),
-        lambda s: (-s[1], -s[0])
+        lambda s: ( s[0],  s[1]), # identity
+        lambda s: (-s[1],  s[0]), # rotate pi/2
+        lambda s: (-s[0], -s[1]), # rotate pi
+        lambda s: ( s[1], -s[0]), # rotate 3pi/2
+        lambda s: (-s[0],  s[1]), # reflect in x axis
+        lambda s: ( s[0], -s[1]), # reflect in y axis
+        lambda s: ( s[1],  s[0]), # reflect in y = x
+        lambda s: (-s[1], -s[0])  # reflect in y = -x
     ]
     return frozenset(
         normalize({t(s) for s in representation}) for t in transformations
     )
+
+# It's useful to be able to get a single representation for a given polyomino.
+
+def represent(polyomino):
+    return next(iter(polyomino))
+
+# We can build (n + 1)-ominoes by adding squares to n-ominoes
+
+def grow(polyomino):
+    directions = [(1, 0), (0, 1), (-1, 0), (0, -1)]
+    representation = represent(polyomino)
+    for (x, y) in representation:
+        for (dx, dy) in directions:
+            if (x + dx, y + dy) not in representation:
+                yield newPolyomino(representation.union({(x + dx, y + dy)}))
+
+# This is everything we need to generate the set of all n-ominoes...
+
+def polyominoes(n):
+    ominoes = {1: {newPolyomino({(0, 0)})}}
+    for i in range(1, n):
+        ominoes[i + 1] = {a for b in ominoes[i] for a in grow(b)}
+    return ominoes[n]
