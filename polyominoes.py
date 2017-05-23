0b9c047e648d93c57613a941048608efbaee7b2b
Callum Oakley <callum@pusher.com>
2017-05-23 23:20:48 +0100

    remove drawing functions

--- polyominoes.py
+++ polyominoes.py
@@ -2,7 +2,6 @@
 # the corresponding polyominoes constituent squares. A representation is
 # normalized if the co-ordinates of all of its points are non-negative, at
 # least one x co-ordinate is 0 and at least one y co-ordinate is 0.
-
 def normalize(representation):
     dx = min(x for x, _ in representation)
     dy = min(y for _, y in representation)
@@ -10,7 +9,6 @@ def normalize(representation):
 
 # We'll store a polyomino as a set of the normalized representations of all its
 # rotations and reflections.
-
 def newPolyomino(representation):
     return frozenset(normalize(r) for r in [
         {( x,  y) for x, y in representation}, # identity
@@ -24,12 +22,10 @@ def newPolyomino(representation):
     ])
 
 # It's useful to be able to get a single representation for a given polyomino.
-
 def represent(polyomino):
     return set(next(iter(polyomino)))
 
 # We can build (n + 1)-ominoes by adding squares to n-ominoes
-
 def grow(polyomino):
     representation = represent(polyomino)
     return (newPolyomino(representation.union({s})) for s in {
@@ -40,27 +36,8 @@ def grow(polyomino):
     })
 
 # This is everything we need to generate the set of all n-ominoes...
-
 def polyominoes(n):
     ominoes = {1: {newPolyomino({(0, 0)})}}
     for i in range(1, n):
         ominoes[i + 1] = {a for b in ominoes[i] for a in grow(b)}
     return [represent(p) for p in ominoes[n]]
-
-# Let's also provide a function to draw them, just for fun
-
-def draw(representation):
-    for x in range(len(representation)):
-        for y in range(len(representation)):
-            if (x, y) in representation:
-                print("#", end=" ")
-            else:
-                print("·", end=" ")
-        print()
-
-# Or to draw the whole set
-
-def drawBatch(representations):
-    for r in representations:
-        draw(r)
-        print()
